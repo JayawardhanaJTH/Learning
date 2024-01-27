1475478249888ca9690a59b6125945888bb83784
JayawardhanaJTH <tiranharsha2323@gmail.com>
2024-01-28 00:33:07 +0530

    add lesson 2

---
+++ Learning/Lessons/System.Text.Json/L2/L2Main.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Learning.Lessons.System.Text.Json.L2
+{
+    public class L2Main
+    {
+        public record Person(string FirstName, string LastName);
+
+        /// <summary>
+        /// .NET 8 Feature
+        /// Convert the snake case json properties to the given policy
+        /// (snake case -> lower)
+        /// </summary>
+        public static void Execute()
+        {
+            var json = """
+                {
+                    "first_name":"Test",
+                    "last_name":"Test Last"
+                }
+                """;
+
+            var jsonOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower
+            };
+
+            var person = JsonSerializer.Deserialize<Person>(json, jsonOptions);
+
+            //print on console
+            Console.WriteLine("Lesson 2 {0}", person);
+            /*
+             *  Person { FirstName = Test, LastName = Test Last }
+             */
+        }
+    }
+}
--- Learning/Program.cs
+++ Learning/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿using Learning.Lessons.System.Text.Json.L1;
+using Learning.Lessons.System.Text.Json.L2;
+
+//Lesson 1
+L1Main.Execute();
+
+//Lesson 2
+L2Main.Execute();
