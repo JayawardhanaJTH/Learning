461b5301c17040dbb8f9c0a67ae43a5d80bc5c22
JayawardhanaJTH <tiranharsha2323@gmail.com>
2024-01-28 00:48:56 +0530

    add lesson 3

---
+++ Learning/Lessons/System.Text.Json/L3/L3Main.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Learning.Lessons.System.Text.Json.L3
+{
+    public class L3Main
+    {
+        /// <summary>
+        /// This will throw an exception because the Name property is not a member of
+        /// Person class and the policy is setted to Disallow
+        /// First pirority to policy under class definition
+        /// Second pirority to json policy options
+        /// </summary>
+        public static void Execute()
+        {
+            var json = """
+                {
+                    "Id": 1,
+                    "Name": "Test"
+                }
+                """;
+
+            var jsonOptions = new JsonSerializerOptions
+            {
+                UnmappedMemberHandling = JsonUnmappedMemberHandling.Disallow
+            };
+
+            var person = JsonSerializer.Deserialize<Person>(json, jsonOptions);
+            /*
+             * System.Text.Json.JsonException: 
+             * 'The JSON property 'Name' could not be mapped to any .NET member contained in type 
+             * 'Learning.Lessons.System.Text.Json.L3.Person'.'
+             */
+        }
+    }
+
+    [JsonUnmappedMemberHandling(JsonUnmappedMemberHandling.Disallow)]
+    public class Person
+    {
+        public int Id { get; set; }
+    }
+}
--- Learning/Program.cs
+++ Learning/Program.cs
@@ -1,8 +1,12 @@
 ﻿using Learning.Lessons.System.Text.Json.L1;
 using Learning.Lessons.System.Text.Json.L2;
+using Learning.Lessons.System.Text.Json.L3;
 
 //Lesson 1
 L1Main.Execute();
 
 //Lesson 2
 L2Main.Execute();
+
+//Lesson 3
+L3Main.Execute();
